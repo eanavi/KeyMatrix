b16b599031facde1bd6bdb116d9dbbb402512cd2
Elvis Anavi <eanavi@gmail.com>
2022-11-01 22:46:57 -0400

    Inicio proyecto KeyMatrix

---
+++ src/main/java/com/prgmtc/keymatrix/KeyMatrix.java
@@ -0,0 +1,16 @@
+
+package com.prgmtc.keymatrix;
+
+import vista.Login;
+
+/**
+ *
+ * @author Elvis
+ */
+public class KeyMatrix {
+
+    public static void main(String[] args) {
+        Login lg = new Login();
+        lg.setVisible(true);
+    }
+}
